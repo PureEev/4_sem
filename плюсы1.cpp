0acd0232f1d17d643027edbae7113ec62878120f
PureEvilToTheCore <132357287+PureEvilToTheCore@users.noreply.github.com>
2024-03-02 10:44:00 +0300

    Create плюсы1.cpp

---
+++ плюсы1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+void AddingSpace(int NumberOfSpaces, string NewLine, int CountOfCurCharInLine) {
+	if (NumberOfSpaces > 1) {
+		while (NumberOfSpaces > 0) {
+			NewLine.append(" ");
+			NumberOfSpaces--;
+			CountOfCurCharInLine++;
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	string FileName = argv[1];
+	int NewLengthOfLine = int(argv[2]);
+	vector<string> NewLines;
+	string baseline;
+	string RemainsOfPrevLine;
+	string NewLine;
+	int NumberOfSpaces = 0;
+	int LengthOfCurrentLine = 0;
+	int CountOfCurCharInLine = 0;
+	ifstream FILE(FileName);
+	while (getline(FILE, baseline)) {
+		if (RemainsOfPrevLine.length() != 0) {
+			NewLine.append(RemainsOfPrevLine);
+			RemainsOfPrevLine = "";
+			LengthOfCurrentLine += NewLine.length() + 1;
+		}
+		istringstream Line(baseline);
+		istream_iterator<string> begin(Line);
+		istream_iterator<string> end;
+		vector<string> words(begin, end);
+		LengthOfCurrentLine += baseline.length();
+		NumberOfSpaces = LengthOfCurrentLine - NewLengthOfLine;
+		AddingSpace(NumberOfSpaces, NewLine, CountOfCurCharInLine);
+		for (const string& word : words) {
+			if ((CountOfCurCharInLine + word.length()) > NewLengthOfLine) break;
+			NewLine.append(word);
+			NewLine.append(" ");
+		}
+		AddingSpace(NumberOfSpaces, NewLine, CountOfCurCharInLine);
+		NewLines.push_back(NewLine);
+	}
+	ofstream file(argv[2]);
+	for (const string& line : NewLines) {
+		file<< line <<endl;
+	}
+	return 0;
+}
+
